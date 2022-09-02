da49de1c56bfd3a612c7bce7a19f2ae219ac05c8
[Anibal-programa] <[cursoprogramadorweb2022@gmail.com]>
2022-09-02 12:02:13 -0300

    Actualización

--- apps/noticias_app/views.py
+++ apps/noticias_app/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import NoticiaForm, CommentarioForm
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import( CreateView)
-
+from django.shortcuts import render # agregue ultimo
 
 # Create your views here.
 
@@ -17,7 +17,7 @@ def noticias(request):
     noticias_app=Noticia, Categoria, Comentarios.objects.all()
     return render(request, "apps.noticias_app/noticias.html",{"noticias_app":noticias_app})
 ''' 
-
+'''
 def index(request):
     #texto = {'mensaje_texto': 'Esta es mi primer pagina :)'}
     ultimasnoticias = Noticia.objects.all().order_by('creado').reverse()[:3]
@@ -25,6 +25,7 @@ def index(request):
         'noticiasdestacadas':ultimasnoticias
     }
     return render(request, 'index.html',context)
+'''
 '''
 def nosotros(request):
     return render(request, 'nosotros.html',{})
@@ -36,7 +37,7 @@ def noticias(request):
         "noticias": lista_noticias,
         #"MEDIA_ROOT": 'media/img/noticias/'
     }
-    return render(request, 'apps/noticias_app/templates/apps.noticias_app/noticias.html',context)
+    return render(request, 'noticias.html',context)
 
 
 def noticiasdetalle(request,id):
@@ -45,7 +46,7 @@ def noticiasdetalle(request,id):
         lista_comentarios = Comentarios.objects.filter(aprobado=True)
     except Noticia.DoesNotExist:
         raise Http404('La Noticia solicitada no existe')
-'''
+
     form=CommentarioForm()
     if request.method=='POST':
         form = CommentarioForm(request.POST)
@@ -70,7 +71,7 @@ def noticiasdetalle(request,id):
 
     return render(request,'detalle-noticia.html',context)
 
-'''
+
 
 '''
 class CrearNoticiaView(CreateView, LoginRequiredMixin):
@@ -94,13 +95,13 @@ class CrearNoticiaView(CreateView, LoginRequiredMixin):
         }
         return render(request, "blog_categoria.html", context)
         
-
+'''
 @login_required
 def post_publish(request, id):
     try:
         noticias =Noticia.objects.get(id =id)
     except Noticia.DoesNotExist:
-        raise Http404('No existe la noticia')
+        raise Http404('No existe la noticia seleccionada')
     
     Noticia.publish()
     return redirect('detalle-noticia', id=id)
@@ -111,7 +112,7 @@ def comment_approve(request, id):
     try:
         comentarios =Comentarios.objects.get(id =id)
     except Comentarios.DoesNotExist:
-        raise Http404('Comentario no existe')
+        raise Http404('El comentario no existe')
     comentarios.approve()
     return redirect('detalle-noticia', id=comentarios.noticia.id)
 
@@ -121,8 +122,7 @@ def comment_remove(request, id):
     try:
         comentario =Comentarios.objects.get(id =id)
     except Comentarios.DoesNotExist:
-        raise Http404('Comentario no existe')
+        raise Http404('El comentario no existe')
     noticia_id = comentario.noticia.id
     comentario.delete()
-    return redirect('noticia_detalle', id=noticia_id)   
-'''
+    return redirect('noticia_detalle', id=noticia_id)
